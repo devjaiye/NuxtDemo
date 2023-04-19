d0a70d7c0114f529e9b69de410c91a358b5c1fe4
devjaiye <dayojaiye74@gmail.com>
2023-04-19 14:06:45 +0100

    fetch product details

--- product-app/pages/products/[id].vue
+++ product-app/pages/products/[id].vue
@@ -1,9 +1,6 @@
 <template>
     <div>
-        <p>{{ product.id }}</p>
-        <p>{{ product.title }}</p>
-        <p>{{ product.price }}</p>
-       
+        <ProductDetails :product="product"/>
     </div>
 </template>
 
